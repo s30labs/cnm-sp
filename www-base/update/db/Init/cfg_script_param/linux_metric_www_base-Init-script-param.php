5ba34d4e5904e279137f0b96f7224933ea7e16ac
Fernando Marin <fmarin@s30labs.com>
2020-05-06 18:41:21 +0200

    [IMP] www-base provision layout updated

--- www-base/update/db/Init/cfg_script_param/linux_metric_www_base-Init-script-param.php
+++ www-base/update/db/Init/cfg_script_param/linux_metric_www_base-Init-script-param.php
@@ -2,13 +2,19 @@
       $CFG_SCRIPT_PARAM[]=array(
 	      'hparam' => '9f33409e', 'position' => '0', 'param_type' => '2',
    	   'script' => 'linux_metric_www_base.pl',
-      	'prefix' => '-ip', 'descr' => 'IP', 'value' => '',
+      	'prefix' => '-ip', 'descr' => 'IP Address', 'value' => '',
       );
 
       $CFG_SCRIPT_PARAM[]=array(
 	      'hparam' => '2104afb0', 'position' => '1', 'param_type' => '0',
    	   'script' => 'linux_metric_www_base.pl',
-      	'prefix' => '-pattern', 'descr' => 'PATRON', 'value' => '',
+      	'prefix' => '-u', 'descr' => 'URL', 'value' => '',
+      );
+
+      $CFG_SCRIPT_PARAM[]=array(
+	      'hparam' => '5aceeb79', 'position' => '2', 'param_type' => '0',
+   	   'script' => 'linux_metric_www_base.pl',
+      	'prefix' => '-pattern', 'descr' => 'Pattern', 'value' => '',
       );
 
 ?>
